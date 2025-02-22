cbecf30d3638f9794a7c9a71281ba959fb06272d
Sergey <domarch388640@gmail.com>
2025-02-22 14:30:51 +0200

    JS lern

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // alert('GREETING');
+
+const person = 'Joe';
+let i = 0;
+
+while (i < 2) {
+		console.log('Happy Birthday to you');
+	i += 1;
+}
+
+	console.log(`Happy Birthday dear ${person},`);
+	console.log('Happy Birthday to you');
