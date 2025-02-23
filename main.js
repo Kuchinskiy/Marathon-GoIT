87d764ed9237c03295b29292e24e312578170dd6
Sergey <domarch388640@gmail.com>
2025-02-23 16:37:32 +0200

    js new

--- main.js
+++ main.js
@@ -2,13 +2,31 @@
 
 // alert('GREETING');
 
-const person = 'Joe';
-let i = 0;
+// const person = 'Joe';
+// let i = 0;
 
-while (i < 2) {
-		console.log('Happy Birthday to you');
-	i += 1;
+// while (i < 2) {
+// 		console.log('Happy Birthday to you');
+// 	i += 1;
+// }
+
+// 	console.log(`Happy Birthday dear ${person},`);
+// 	console.log('Happy Birthday to you');
+
+
+const word = 'bottles';
+let count = 5;
+
+while (count > 0) {
+	console.log(`${count} ${word} of beer on the wall`);
+	console.log(`${count} ${word} of beer,`);
+	console.log('Take one down, pass it around,');
+
+	count -= 1;
+
+	if (count > 0) {
+		console.log(`${count} ${word} of beer on the wall.`);
+	} else {
+		console.log(`No more ${word} No more`);
+	}
 }
-
-	console.log(`Happy Birthday dear ${person},`);
-	console.log('Happy Birthday to you');
